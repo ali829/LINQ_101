f11d40a493c0193a2ade758569d42891ead591c5
ali829 <alielyassraoui@gmail.com>
2024-07-16 12:08:35 +0100

    complete creating winForm

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,40 @@
+﻿using LinqLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        /* order by property */
+        List<Person> people = ListManager.LoadSampleData();
+        people = people.OrderBy(x => x.YearsExperience).ToList();
+
+        /* order by property Descending*/
+        people = people.OrderByDescending(x => x.YearsExperience).ToList();
+
+        /*second priority ordering*/
+        people = people.OrderBy(x => x.LastName).ThenBy(x => x.YearsExperience).ToList();
+
+        /* filtering list */
+        List<Person> people1 = ListManager.LoadSampleData();
+        people1 = people1.Where(x => x.YearsExperience > 2 && x.Birthday.Month == 3).ToList();
+
+        foreach (var person in people1)
+        {
+        Console.WriteLine($"{ person.FirstName } { person.LastName } ({ person.Birthday.ToShortDateString() }): Experience { person.YearsExperience }");
+        }
+
+            /* totale sum method*/
+            var totalExp = people1.Sum(x => x.YearsExperience);
+            Console.WriteLine($"The total of experiences is {totalExp}");
+
+      Console.ReadLine();
+    }
+  }
+}
---
+++ FormUI/Dashboard.cs
@@ -0,0 +1,64 @@
+﻿using LinqLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormUI
+{
+  public partial class Dashboaard : Form
+  {
+    List<Person> people = ListManager.LoadSampleData();
+
+    public Dashboaard()
+    {
+      InitializeComponent();
+
+      InitializeBindings();
+    }
+
+    private void InitializeBindings()
+    {
+      allPeopleDropDown.DataSource = people;
+      allPeopleDropDown.DisplayMember = "FullName";
+    }
+
+    private void UpdateBindings()
+    {
+        filteredPeopleList.Items.Clear();
+        var selectedExpYears = yearsExperiencePicker.Value;
+        List<Person> filteredPeople = people.Where(p => p.YearsExperience >= selectedExpYears).ToList();
+        foreach (Person person in filteredPeople)
+        {
+                filteredPeopleList.Items.Add(person.FullName);
+        }
+    }
+
+    private void allPeopleDropDown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+      Person selectedPerson = (Person)allPeopleDropDown.SelectedItem;
+
+      yearsExperiencePicker.Value = selectedPerson.YearsExperience;
+    }
+
+    private void updatePersonButton_Click(object sender, EventArgs e)
+    {
+      UpdateBindings();
+    }
+
+        private void Dashboaard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
